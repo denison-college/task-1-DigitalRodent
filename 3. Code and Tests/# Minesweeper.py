6024d9198acd5d9a8c8bab940c12b79080044a96
DigitalRodent <rendaroadkill@gmail.com>
2026-04-16 13:36:11 +1000

    Finsihing up main menu functions

--- 3. Code and Tests/# Minesweeper.py
+++ 3. Code and Tests/# Minesweeper.py
@@ -1,50 +1,38 @@
 # Import area
-import rich
+# import rich
 import time
 import sys 
 import os
 
+def text_print(text, delay):
+    for char in text:
+        sys.stdout.write(char)
+        sys.stdout.flush()
+        time.sleep(delay)
+
 def play():
     os.system('clear')
-    glad = "We are so glad you're here..."
-    for con in glad:
-        sys.stdout.write(con)
-        sys.stdout.flush()
-        time.sleep(0.1)
+    text_print("We are so glad you're here...", 0.1)
     time.sleep(1)
     print('\n')
-    please = "Please fill out the form below to continue"
-    for gink in please:
-        sys.stdout.write(gink)
-        sys.stdout.flush()
-        time.sleep(0.1)
+    text_print("Please fill out the form below to continue", 0.1)
     time.sleep(1)
 
     print('\n')
     print('''
     ______________________________________________________________________________________________
-    |                              Legally Binding Consent form                                  |
-    ==============================================================================================
-    |                                                                                            |
-    | First Name:                                                                                |
-    |                                                                                            |
-    |                                                                                            |
-    |                                                                                            |
-    |                                                                                            |
-    |                                                                                            |
-    |                                                                                            |
-    |                                                                                            |
-    |                                                                                            |
-    |                                                                                            |
-    |                                                                                            |
-    |                                                                                            |
-    |                                                                                            |
-    |                                                                                            |
-    |                                                                                            |
-    |                                                                                            |
-    |                                                                                            |
-    |                                                                                            |
-    |____________________________________________________________________________________________|''')
+                                   Legally Binding Consent form                                   
+    ==============================================================================================''')
+
+    FN = input('First Name: ')
+    SN = input('Surname Name: ')
+    print('\n')
+    DOB = input('Date of Birth: ')
+    print('\n')
+    COO = input('Country of Origin: ')
+    PC = input('Postal Code: ')
+    print('\n')
+    print('\n')
 
 #Minesweeper
 #welcome and options page 1
@@ -67,14 +55,11 @@ def startup2():
     print('Thank you for willingly participating')
     print('\n')
 def startup():
-    intro = 'Welcome to'
-    for char in intro:
-        sys.stdout.write(char)
-        sys.stdout.flush()
-        time.sleep(0.1)
+    text_print("Welcome to", 0.2)
     time.sleep(1)
+
     print('\n')
-    title = (r'''
+    text_print(r'''
     888b     d888 d8b                                                                                      
     8888b   d8888 Y8P                                                                                      
     88888b.d88888                                                                                          
@@ -85,18 +70,11 @@ def startup():
     888       888 888 888  888  "Y8888   88888P'  "Y8888888P"   "Y8888   "Y8888  88888P"   "Y8888  888     
                                                                                  888                       
                                                                                  888                       
-                                                                                 888                       ''')
-    for t1 in title:
-        sys.stdout.write(t1)
-        sys.stdout.flush()
-        time.sleep(0.001)
+                                                                                 888                       ''', 0.001)
+
     print('\n')
     time.sleep(2)
-    intro2 = 'Thank you for willingly participating'
-    for bark in intro2:
-        sys.stdout.write(bark)
-        sys.stdout.flush()
-        time.sleep(0.1)
+    text_print('Thank you for willingly participating', 0.1)
     print('\n')
     time.sleep(0.5)
 def mainmenu():
@@ -106,19 +84,47 @@ def mainmenu():
     print('3. Exit')
     print('\n')
 
-    Option = input('Choose an option:')
+#Options
+    Option = input('Please choose an option:')
     if Option == '1':
         play()
     elif Option == '2':
         quit()
+#Quitting system
+# Y options
     elif Option == '3':
-        quit = 'Now exiting the program'
-        for bink in quit:
-            sys.stdout.write(bink)
-            sys.stdout.flush()
-            time.sleep(0.1)
-        time.sleep(2)
-        quit()
+        text_print('Really? ', 0.1)
+        Option2 = input ( 'Y/N ')
+        if Option2 == 'Y':
+            text_print('Are you sure? ' , 0.1)
+        elif Option2 == 'y':
+            text_print('Are you sure? ' , 0.1)
+def Quit_Option():
+    Option3 = input ( 'Y/N ')
+        if Option3 == 'Y':
+            print('Fine')
+            time.sleep(0.5)
+            text_print("Now Exiting the program...", 0.2)
+            time.sleep(2)
+        elif Option3 == 'y':
+            print('Fine')
+            time.sleep(0.5)
+            text_print("Now Exiting the program...", 0.2)
+            time.sleep(2)
+            quit()
+#N options
+        elif Option3 == 'N':
+            print('Marvelous!')
+            time.sleep(3)
+            os.system('clear')
+            startup2()
+            mainmenu()
+    elif Option2 == 'N':
+        print('Fantastic!')
+        time.sleep(3)
+        os.system('clear')
+        startup2()
+        mainmenu()
 
     #user puts in invalid request
     else:
