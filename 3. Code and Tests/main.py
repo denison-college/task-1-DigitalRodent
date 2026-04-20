96cbdb25e9ab080f874aaf1311e10ed331d37249
DigitalRodent <rendaroadkill@gmail.com>
2026-04-20 21:37:27 +1000

    Finalising things

--- 3. Code and Tests/main.py
+++ 3. Code and Tests/main.py
@@ -1,43 +1,21 @@
 # Import Modules
 import rich
 import time
-import sys 
+import sys
 import os
 
+
+# Functions
 def text_print(text, delay):
     for char in text:
         sys.stdout.write(char)
         sys.stdout.flush()
         time.sleep(delay)
 
-def play():
-    os.system('clear')
-    text_print("We are so glad you're here...", 0.1)
-    time.sleep(1)
-    print('\n')
-    text_print("Please fill out the form below to continue", 0.1)
-    time.sleep(1)
+# Text
 
-    print('\n')
-    print('''
-    ______________________________________________________________________________________________
-                                   Legally Binding Consent form                                   
-    ==============================================================================================''')
-
-    FN = input('First Name: ')
-    SN = input('Surname Name: ')
-    print('\n')
-    DOB = input('Date of Birth: ')
-    print('\n')
-    COO = input('Country of Origin: ')
-    PC = input('Postal Code: ')
-    print('\n')
-    print('\n')
-
-#Minesweeper
-#welcome and options page 1
-time.sleep(2)
-def startup2():
+def title2():
+    time.sleep(2)
     print('Welcome to')
     print(r''' 
     
@@ -54,7 +32,9 @@ def startup2():
                                                                                  888                       '''.center(100, ' '))
     print('Thank you for willingly participating')
     print('\n')
-def startup():
+
+
+def title():
     text_print("Welcome to", 0.2)
     time.sleep(1)
 
@@ -77,6 +57,8 @@ def startup():
     text_print('Thank you for willingly participating', 0.1)
     print('\n')
     time.sleep(0.5)
+
+
 def mainmenu():
     print('Options')
     print('1. Play Game')
@@ -84,46 +66,23 @@ def mainmenu():
     print('3. Exit')
     print('\n')
 
-#Options
-    Option = input('Please choose an option:')
+    Option = input('Please choose an option: ')
     if Option == '1':
-        play()
+        os.system('cls')
+        os.system('python3 game.py')
     elif Option == '2':
         quit()
-#Quitting system
-# Y options
     elif Option == '3':
-        text_print('Really? ', 0.1)
-        Option2 = input ( 'Y/N ')
-        if Option2 == 'Y':
-            text_print('Are you sure? ' , 0.1)
-def Quit_Option():
-    Option3 = input ( 'Y/N ')
-        if Option3 == 'Y':
-            print('Fine')
-            time.sleep(0.5)
             text_print("Now Exiting the program...", 0.2)
-            time.sleep(2)
-#N options
-        elif Option3 == 'N':
-            print('Marvelous!')
-            time.sleep(3)
-            os.system('clear')
-            startup2()
-            mainmenu()
-    elif Option2 == 'N':
-        print('Fantastic!')
-        time.sleep(3)
-        os.system('clear')
-        startup2()
-        mainmenu()
+            time.sleep(0.5)
+            quit()
 
-    #user puts in invalid request
     else:
         print('Invalid Option')
         time.sleep(3)
-        os.system('clear')
-        startup2()
+        os.system('cls')
+        title2()
         mainmenu()
-startup()
+
+title()
 mainmenu()
